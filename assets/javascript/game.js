03ab75f6886a87d810e8eba1abcca8414ac6d9fa
Ben Garcia <ben@seebenprogram.com>
2018-01-26 13:52:23 -0500

    Comment HTML elem id's for convenience

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+/*
+ * For convenience:
+ * ======================
+ * id's for html elements
+ * ======================
+ * js-insert-img
+ * js-hangman-word
+ * js-guessess-remaining
+ * js-letters-guessed
+ * js-win-count
+ * js-loss-count
+ */
