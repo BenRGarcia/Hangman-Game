129d76b5abd60f0dab99681ce3406ce035058834
Ben Garcia <ben@seebenprogram.com>
2018-01-26 14:41:51 -0500

    Begin to declare variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,4 +11,21 @@
  * js-loss-count
  */
 
- 
+// Variables
+let guessesRemaining = 10;
+let winCount = 0;
+let lossCount = 0;
+let lettersGuessed = [];
+
+const alphabet = [
+"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
+"N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+];
+
+const composers = ["CHOPIN", "BACH", "BEETHOVEN"];
+
+const composers = [
+  {name: "CHOPIN",    imagePath: "./assets/images/stock-img.jpg"},
+  {name: "BACH",      imagePath: "./assets/images/stock-img.jpg"},
+  {name: "BEETHOVEN", imagePath: "./assets/images/stock-img.jpg"}
+];
