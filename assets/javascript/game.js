bf582e2a637bcfa47b9eb12e1c2c32b88ddfd641
Ben Garcia <ben@seebenprogram.com>
2018-01-29 15:25:04 -0500

    Add psuedocode to controller

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -57,6 +57,16 @@ const gameProps = {
     }
   },
 
+  guessHandler(guess) {
+    let word = this.wordBank.word;
+    let j = word.length;
+    for (let i = 0; i < j; i++) {
+      if (guess === word[i]) {
+        this._underscoreWord[i] = guess;
+      }
+    }
+  },
+
   set wordBankObject(obj) {
     this._wordBankObject = obj;
   },
@@ -90,8 +100,8 @@ const gameProps = {
     return this._lettersGuessed;
   },
 
-  get wordBankObject() {
-    return this._wordBankObject.name;
+  get wordBankWord() {
+    return this._wordBankObject.word;
   },
 
   get alphabet() {
@@ -104,6 +114,27 @@ const gameProps = {
 
   get imageAlt() {
     return this._wordBankObject.alt;
+  },
+
+  isGuessValid(guess) {
+    if (this._alphabet.indexOf(guess) !== -1 && 
+        this._lettersGuessed.indexOf(guess) === -1) 
+    {
+      return true;
+    } 
+    else 
+    {
+      return false;
+    }
+  },
+
+  isGuessRight(guess) {
+    let hangmanWord = this.wordBankWord;
+    if (hangmanWord.indexOf(guess) !== -1) {
+      return true;
+    } else {
+      return false;
+    }
   },
 
   decrementGuessesRemaining() {
@@ -121,16 +152,8 @@ const gameProps = {
   newRoundGameProps() {
     this._guessesRemaining = 10;
     this._lettersGuessed = [];
-  },
-
-  guessHandler(guess) {
-    let word = this._wordBankObject.word;
-    let j = word.length;
-    for (let i = 0; i < j; i++) {
-      if (guess === word[i]) {
-        this._underscoreWord[i] = guess;
-      }
-    }
+    this._underscoreWord = ""; // <-- superfluous?
+    this._wordBankObject = {}; // <-- superfluous?
   }
 };
 
@@ -153,6 +176,10 @@ const DOM = {
         document.getElementById('js-guesses-remaining').innerHTML = gameProps.guessesRemaining;
         break;
 
+      case 'underscoreWord':
+        document.getElementById('js-hangman-word').innerHTML = gameProps.underscoreWord;
+        break;
+
       case 'winCount':
         document.getElementById('js-win-count').innerHTML = gameProps.winCount;
         break;
@@ -164,6 +191,14 @@ const DOM = {
       case 'image':
         document.getElementById('js-insert-img').src = gameProps.imageSrc;
         document.getElementById('js-insert-img').alt = gameProps.imageAlt;
+        break;
+
+      case 'newRound':
+        document.getElementById('js-letters-guessed').innerHTML = gameProps.lettersGuessed;
+        document.getElementById('js-hangman-word').innerHTML = gameProps.underscoreWord;
+        document.getElementById('js-guesses-remaining').innerHTML = gameProps.guessesRemaining;
+        document.getElementById('js-win-count').innerHTML = gameProps.winCount;
+        document.getElementById('js-loss-count').innerHTML = gameProps.lossCount;
         break;
 
       default:
@@ -180,42 +215,89 @@ const DOM = {
  *  Controller - accepts input and converts it to commands for the model/view
  */
 
+//    gameEngine's purpose:
+// 1) interaction with game props object
+// 2) interaction with hangman word bank
+// 3) interaction with rendering
+
 const gameEngine = {
 
-  // interaction with game props object
-      // 
-
-
-
-  // interaction with hangman word bank
-
-
-
-  // interaction with rendering
-
-
-
   controller(guess) {
+
+    // Test for being a letter of alphabet, not already guessed
+    if (gameProps.isGuessValid(guess)) {
+
+      // Test for if letter guessed is right
+      if (gameProps.isGuessRight(guess)) {
+        // replace index/indices of underscore word, render updated underscore word
+
+      } else {
+        // add letter guessed to lettersGuessed array
+        // decrement guessesRemaining
+        // render updated guessesRemaining # and lettersGuessed array
+      }
+    }
+
+    // test for win
+    if (false/* underscore word = hangman word */) {
+      // alert user of won game
+      // increment win game
+      // render image src/alt to page, queue music/sound
+    }
+
+    // Test for loss/game over
+    if (false/* guesses remaining = 0 */) {
+      // alert user of lost game
+      // increment loss count
+      // call this.newRound();
+      // 
+    }
+
+
+
     gameProps.lettersGuessed = guess;
     let secretWordObj = hangmanWords.wordBank;
     gameProps.underscoreWord = secretWordObj;
-    if () {
+    if (false) {
 
     }
   },
 
-  generateHangmanWord() {
-    let wordObj = hangmanWords.wordBank;
-    return wordObj;
+  // Setup gameProps to be ready for new round of gameplay
+  newRound() {
+    gameProps.newRoundGameProps();       // guessesRemaining = 10, lettersGuessed = []
+    let wordObj = hangmanWords.wordBank; // generate new hangman word
+    gameProps.wordBankObject = wordObj;  // add new hangman word to gameProps
+    gameProps.underscoreWord = wordObj;  // set underscore word
+    DOM.render('newRound');              // render gameProps to DOM
   }
 };
 
+ /*
+  * step 1 - when page first loads: get new hangman word, pass hangman word to gameprops,
+  *          render underscore word to DOM
+  * 
+  * step 2 - listen for keypress events
+  * 
+  * step 3 - when keypress received, a) if it's a letter b) not previously guessed
+  *          check if in hangman word. If not in hangman word, add to guessed list. 
+  *          if it is in hangman word, update indices of underscore word re-render it.
+  *          Then decrement guesses remaining.
+  * 
+  * step ? - if hangman word not guessed yet and guessesRemaining = 0, alert of lost game,
+  *          increment loss count, reset gameProps: lettersGuessed = [], new hangman word,
+  *          guesses remaining = 10
+  * 
+  * step ? - if underscore word = hangman word, alert of won game, increment win count,
+  *          reset gameProps: lettersGuessed = [], new hangman word, guesses remaining = 10
+  * 
+  * step ? - if 
+  */
 
+/*====================================================================================*/
 
-
-
-
-// Initialize game when page loads the first time
+// Initialize gameProps when page loads the first time
+gameEngine.newRound();
 
 // Event listener
 document.addEventListener('keypress', (event) => {
